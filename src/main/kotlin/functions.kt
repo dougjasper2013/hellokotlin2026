b5abf3d560c5c848163ab3df5f1c45fc212fa8e7
Douglas Jasper <doug.jasper@trios.com>
2026-07-21 10:35:16 -0400

    Added Dice Roll

--- src/main/kotlin/functions.kt
+++ src/main/kotlin/functions.kt
@@ -1,8 +1,18 @@
 package org.example
 
+import  kotlin.random.Random.Default.nextInt
+
 fun main() {
     var intro = printName("Daffy Duck") // calling or invoking the function
     println(intro)
+
+    var dice = rollDice()
+    println(dice)
+
+    for(i in 1..100) {
+        dice = rollDice()
+        println(dice)
+    }
 }
 
 fun printName(name: String): String { // function definition
@@ -10,4 +20,9 @@ fun printName(name: String): String { // function definition
     var greeting = "Hi, $name! How are you?"
 
     return greeting
+}
+
+fun rollDice(): Int {
+    val roll = nextInt(1, 7)
+    return roll
 }
